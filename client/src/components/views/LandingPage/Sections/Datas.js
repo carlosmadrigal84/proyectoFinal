22b05faff9ac6fbb39ca9e64aa869f1811e9bef6
Carlos Madrigal <madrigal.informatica@gmail.com>
2020-02-22 15:53:26 +0100

    Subo proyecto

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,72 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "0€ to 100€",
+        "array": [0, 100]
+    },
+    {
+        "_id": 2,
+        "name": "101€ to 200€",
+        "array": [101, 200]
+    },
+    {
+        "_id": 3,
+        "name": "201€ to 400€",
+        "array": [201, 400]
+    },
+    {
+        "_id": 4,
+        "name": "401€ to 600€",
+        "array": [401, 600]
+    },
+    {
+        "_id": 5,
+        "name": "More than 600",
+        "array": [601, 1500000]
+    }
+]
+
+
+
+const wallathings = [
+    {
+        "_id": 1,
+        "name": "Motor"
+    },
+    {
+        "_id": 2,
+        "name": "Shoes"
+    },
+    {
+        "_id": 3,
+        "name": "Mobile"
+    },
+    {
+        "_id": 4,
+        "name": "Sports"
+    },
+    {
+        "_id": 5,
+        "name": "Electronics"
+    },
+    {
+        "_id": 6,
+        "name": "Clothes"
+    },
+    {
+        "_id": 7,
+        "name": "Decoration"
+    }
+]
+
+
+export {
+    price,
+    wallathings
+}
